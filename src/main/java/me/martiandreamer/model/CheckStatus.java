8dd26f6d9f8de10bb96acca1b2448beea792343e
MartianDreamer <nguyenxuansang9494@gmail.com>
2024-04-17 21:34:54 +0700

    update status

--- src/main/java/me/martiandreamer/model/CheckStatus.java
+++ src/main/java/me/martiandreamer/model/CheckStatus.java
@@ -1,5 +1,17 @@
 package me.martiandreamer.model;
 
-public record CheckStatus(String previousAction) {
+public record CheckStatus(String previousAction,
+                          String employeeName,
+                          String day,
+                          int intime,
+                          int outtime,
+                          int workedTime,
+                          int morningTime,
+                          int afternoonTime,
+                          int lastStartBreak,
+                          int lastEndBreak,
+                          String informMessages,
+                          int type
+) {
     public static String CHECKIN = "CHECKIN";
 }
