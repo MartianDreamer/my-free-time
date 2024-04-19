1cb8aac655f1c737a3a9b65a521c5fa52159c58c
MartianDreamer <nguyenxuansang9494@gmail.com>
2024-04-19 18:28:09 +0700

    v1.3.0-SNAPSHOT send email

--- src/main/java/me/martiandreamer/control/MailerService.java
+++ src/main/java/me/martiandreamer/control/MailerService.java
@@ -5,12 +5,14 @@ import io.quarkus.mailer.Mailer;
 import jakarta.enterprise.context.ApplicationScoped;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import me.martiandreamer.model.Configuration;
 
 import java.time.LocalDateTime;
 
 @ApplicationScoped
 @RequiredArgsConstructor
+@Slf4j
 public class MailerService {
     private final Mailer mailer;
     private final Configuration configuration;
@@ -18,14 +20,18 @@ public class MailerService {
 
     public void sendCheckMail(Action action) {
         String now = LocalDateTime.now().format(ScheduledCheckService.DATE_TIME_FORMATTER);
-        mailer.send(
-                Mail.withText(configuration.getEmail(),
-                        "You " + action.getAction() + " at " + now + ".",
-                        "Dear " + communicationService.getWindowsAccount() + ",\n" +
-                                "You " + action.getAction() + " at " + now + ".\n\n" +
-                                "Regards,\n" +
-                                "Your free time.")
-        );
+        try {
+            mailer.send(
+                    Mail.withText(configuration.getEmail(),
+                            "You " + action.getAction() + " at " + now + ".",
+                            "Dear " + communicationService.getWindowsAccount() + ",\n" +
+                                    "You " + action.getAction() + " at " + now + ".\n\n" +
+                                    "Regards,\n" +
+                                    "Your free time.")
+            );
+        } catch (RuntimeException e) {
+            log.error("failed to send check mail", e);
+        }
     }
 
     @RequiredArgsConstructor
